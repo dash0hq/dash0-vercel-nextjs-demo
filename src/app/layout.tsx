2ff0bfd6078832cb2baad3d98e531f43a23ffc80
Ben Blackmore <ben.blackmore@dash0.com>
2024-08-10 21:59:40 +0200

    feat: add Dash0 logo and screenshot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import Image from "next/image";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
-import { Logo } from "@/components/ui/logo";
 import { NavLink } from "@/components/ui/nav-link";
+import dash0Logo from "@/assets/dash0-logo.png";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -26,7 +26,10 @@ export default function RootLayout({
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
           <div className="mx-auto grid w-full max-w-6xl gap-2">
-            <h1 className="text-3xl font-semibold">Dash0: Vercel &amp; Next.js example</h1>
+            <h1 className="flex items-center gap-4 text-3xl font-semibold">
+              <Image src={dash0Logo} alt="Dash0 Logo" width={48} height={48} />
+              Vercel &amp; Next.js example
+            </h1>
           </div>
           <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
             <nav className="grid gap-2 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
+import dash0TraceScreenshot from "@/assets/dash0-trace.png";
 
 export default function Home() {
   return (
@@ -10,17 +11,35 @@ export default function Home() {
           <CardTitle>What is this?</CardTitle>
         </CardHeader>
         <CardContent className="prose dark:prose-invert">
-          This example demonstrates various scenarios a Next.js app might encounter. Observability data is reported to{" "}
-          <Link href="https://www.dash0.com">Dash0</Link> for visualization.
+          <p>
+            This example demonstrates various scenarios a Next.js app deployed on Vercel might encounter. Each scenario
+            is represented by a different page. You can use the navigation on the left to explore each scenario.
+          </p>
+          <p>
+            The app is instrumented with OpenTelemetry via{" "}
+            <Link href="https://github.com/vercel/otel">@vercel/otel</Link> to collect traces, which are then sent to{" "}
+            <Link href="https://www.dash0.com">Dash0</Link> for visualization. Additionally, a Vercel log drain is
+            leveraged to export logging information.
+          </p>
         </CardContent>
       </Card>
 
       <Card>
         <CardHeader>
-          <CardTitle>How is this app configured?</CardTitle>
+          <CardTitle>What does the observability data look like?</CardTitle>
+          <CardDescription>
+            The following screenshot shows a visualized distributed trace for a failed fetch issued by a React server
+            component.
+          </CardDescription>
         </CardHeader>
-        <CardContent className="prose dark:prose-invert">
-          TODO, headers, <code>@vercel/otel</code> etc.
+        <CardContent>
+          <Link href={dash0TraceScreenshot.src}>
+            <Image
+              src={dash0TraceScreenshot}
+              alt="Dash0 trace view showing how a React server-component is rendered"
+              className="rounded-md"
+            />
+          </Link>
         </CardContent>
       </Card>
     </div>
