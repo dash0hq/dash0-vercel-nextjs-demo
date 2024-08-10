aab4b40dcf6f930eff30129aa856094439759e1d
Ben Blackmore <ben.blackmore@dash0.com>
2024-08-10 20:10:08 +0200

    feat: add @vercel/otel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Card x-chunk="dashboard-04-chunk-1">
+    <div className="flex flex-col gap-6">
+      <Card>
         <CardHeader>
           <CardTitle>What is this?</CardTitle>
         </CardHeader>
-          <CardContent className="prose dark:prose-invert">
-              This example demonstrates various scenarios a Next.js app might encounter. Observability data
-              is reported to <Link href="https://www.dash0.com">Dash0</Link> for visualization.
-          </CardContent>
+        <CardContent className="prose dark:prose-invert">
+          This example demonstrates various scenarios a Next.js app might encounter. Observability data is reported to{" "}
+          <Link href="https://www.dash0.com">Dash0</Link> for visualization.
+        </CardContent>
       </Card>
 
-      <Button>Click me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>How is this app configured?</CardTitle>
+        </CardHeader>
+        <CardContent className="prose dark:prose-invert">
+          TODO, headers, <code>@vercel/otel</code> etc.
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/instrumentation.ts
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright 2023 Dash0 Inc.
+
+import { registerOTel } from "@vercel/otel";
+
+export function register() {
+	registerOTel({
+		serviceName: "vercel-nextjs-demo",
+		instrumentationConfig: {
+			fetch: {
+				propagateContextUrls: [],
+			},
+		},
+	});
+}
